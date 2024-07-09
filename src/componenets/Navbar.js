5d00479b4d1e0e8267204049f9c85688df608586
Irving Rabel <irabel-consultant@scholastic.com>
2024-07-09 15:44:43 -0400

    Added tailwind and Navbar

---
+++ src/componenets/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar(){
+    return (
+        <div className="bg-sky-950 w-screen h-16">
+            <img src="../logo.svg" alt="logo"/>
+        </div>
+    )
+}
+
+export default Navbar
